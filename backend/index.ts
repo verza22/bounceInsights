515d9963c2e9aaae1a3c2ddbcfcfafe97ebf0538
Luis Zurita <46327452+verza22@users.noreply.github.com>
2025-07-14 21:57:59 +0200

    testing in production

--- backend/index.ts
+++ backend/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use('/', routes); 
+app.use('/api', routes); 
 
 // WebSocket
 setupWebSocket(server);
--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,4 +1,6 @@
-export const API_URL = "http://localhost:3001/";
+const isProd = process.env.NODE_ENV === 'production';
+
+export const API_URL = isProd ? "https://nasa.luiszurita.es/api/" : "http://localhost:3001/";
+export const WEBSOCKET_URL = isProd ? "wss://nasa.luiszurita.es/api" : "ws://localhost:3001";
 export const API_TIMEOUT = 10000;
-export const WEBSOCKET_URL = "ws://localhost:3001";
 export const ERROR_TIMEOUT = 7500;
